3fae0d53e7e687209b67cc72b7c2e09853b0071a
gibs0nnn <gibs0n@knights.ucf.edu>
2021-09-11 18:59:15 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,61 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 21 Solution
+ *  Copyright 2021 Nathaniel Gibson
+ */
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number of the month: ");
+        int monthNumber = scanner.nextInt();
+        String month = "invalid";
+        switch(monthNumber) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                month = "Invalid Input";
+                break;
+        }
+        System.out.println("The name of the month is " + month + ".");
+    }
+}
